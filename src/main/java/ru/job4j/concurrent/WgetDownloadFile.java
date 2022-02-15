52a5b5c086aee73961c4ec13261930feaf732b8e
AMakutsevi4 <amakutsevich@bk.ru>
2022-02-15 13:36:13 +0900

    4. Скачивание файла с ограничением.

--- src/main/java/ru/job4j/concurrent/WgetDownloadFile.java
+++ src/main/java/ru/job4j/concurrent/WgetDownloadFile.java
@@ -4,39 +4,52 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 public class WgetDownloadFile implements Runnable {
     private final String url;
     private final int speed;
+    private final String nameFile;
 
-    public WgetDownloadFile(String url, int speed) {
+    public WgetDownloadFile(String url, String nameFile, int speed) {
         this.url = url;
+        this.nameFile = nameFile;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte[] dataBuffer = new byte[1024];
+             FileOutputStream fileOutputStream = new FileOutputStream(nameFile)) {
+            byte[] dataBuffer = new byte[speed];
             int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                int time = bytesRead - speed;
-                if (time < 0) {
-                    time = bytesRead + speed;
+                long finish = System.currentTimeMillis();
+                long rsl = finish - start;
+                if (speed > rsl) {
+                    Thread.sleep(speed - rsl);
                 }
-                Thread.sleep(time);
+                start = System.currentTimeMillis();
             }
         } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().isInterrupted();
+        }
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Не установленные все параметры, проверьте скачиваемый файл, имя файла и скорость ограничения");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        validateArgs(args);
         String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new WgetDownloadFile(url, speed));
+        String nameFile = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new WgetDownloadFile(url, nameFile, speed));
         wget.start();
         wget.join();
     }
