f2093dfa8816a4237d7e26022598877f7dd86676
Alexandr <amakutsevich@bk.ru>
2022-02-23 21:26:36 +0900

    1. Неблокирующий кеш

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,26 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        Base base = new Base(model.getId(), model.getVersion() + 1);
+        base.setName(model.getName());
+        return true;
+    }
+
+    public void delete(Base model) {
+       memory.remove(model.getId());
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.cache;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class CacheTest {
+
+    @Test
+    public void add() {
+        Cache cache = new Cache();
+        Base base1 = new Base(1, 0);
+        Base base2 = new Base(2, 1);
+        cache.add(base1);
+        assertTrue(cache.add(base2));
+    }
+
+    @Test
+    public void update() {
+        Cache cache = new Cache();
+        Base base1 = new Base(1, 0);
+        Base base2 = new Base(2, 1);
+        cache.add(base1);
+        cache.add(base2);
+        Base base3 = new Base(2, 1);
+        Base base4 = new Base(2, 2);
+        assertTrue(cache.update(base3));
+        assertFalse(cache.add(base4));
+    }
+
+    @Test
+    public void delete() {
+        Cache cache = new Cache();
+        Base base1 = new Base(1, 0);
+        Base base2 = new Base(2, 1);
+        cache.add(base1);
+        cache.add(base2);
+        cache.delete(base2);
+        assertTrue(cache.add(base2));
+    }
+}
