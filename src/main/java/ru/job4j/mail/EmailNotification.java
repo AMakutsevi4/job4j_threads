eb4ad0b67abba12271122461f3b7a85fb0dfbfff
Alexandr <amakutsevich@bk.ru>
2022-02-28 21:09:33 +0900

    2. ExecutorService рассылка почты.

--- src/main/java/ru/job4j/mail/EmailNotification.java
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -10,11 +10,11 @@ public class EmailNotification {
     );
 
     public void emailTo(User user) {
-        String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
-        String body = String.format("Add a new event to %s", user.getUsername());
         pool.submit(new Runnable() {
             @Override
             public void run() {
+                String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
+                String body = String.format("Add a new event to %s", user.getUsername());
                 send(subject, body, user.getEmail());
             }
         });
